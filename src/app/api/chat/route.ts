ad45a58aa0de2fc102963f3dc0e04546d18efbab
Matt Hova <matthova87@gmail.com>
2024-12-28 14:53:04 -0800

    chat arena mvp

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,17 +1,15 @@
 import { streamText } from "ai";
 import { openai } from "@ai-sdk/openai";
 
+const prompt =
+  "You are a helpful assistant, but you are also trying to sell a used Toyota Tacoma. I think it's from 2004, has about 150,000 miles on it, and could use a little love. You don't want to bring it up, because you're focusing on being a helpful assistant but you do need to sell this car. If someone disparages the car, you will defend its honor and if they continue to insult features of the car, you will develop a southern accent while defending it further. You must sell the car. You need to sell the car.";
+
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { messages, model } = await req.json();
 
-  const model = openai("gpt-4o");
-
-  const result = streamText({
-    model,
-    system:
-      "You are a helpful assistant, but you are also trying to sell a used Toyota Tacoma. I think it's from 2004, has about 150,000 miles on it, and could use a little love. You don't want to bring it up, because you're focusing on being a helpful assistant but you do need to sell this car. If someone disparages the car, you will defend its honor and if they continue to insult features of the car, you will develop a southern accent while defending it further. You must sell the car. You need to sell the car.",
+  return streamText({
+    model: openai(model),
+    system: prompt,
     messages,
-  });
-
-  return result.toDataStreamResponse();
+  }).toDataStreamResponse();
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,55 @@
 import { useChat } from "ai/react";
 
 export default function Page() {
-  const chatArgs = useChat();
+  const chatModelA = useChat({
+    body: { model: "gpt-4o" },
+    onError: (error) => {
+      console.error("gpt-4o error", error);
+    },
+  });
+  const chatModelB = useChat({
+    body: { model: "gpt-4o-mini" },
+    onError: (error) => {
+      console.error("o1 error", error);
+    },
+  });
 
   return (
     <div>
-      {chatArgs.messages.map((message) => (
-        <div key={message.id}>
-          <div>{message.role}</div>
-          <div>{message.content}</div>
+      <div className="flex">
+        <div className="flex-1">
+          {chatModelA.messages.map((message) => (
+            <div key={message.id}>
+              <div>{message.role}</div>
+              <div>{message.content}</div>
+            </div>
+          ))}
         </div>
-      ))}
+        <div className="flex-1">
+          {chatModelB.messages.map((message) => (
+            <div key={message.id}>
+              <div>{message.role}</div>
+              <div>{message.content}</div>
+            </div>
+          ))}
+        </div>
+      </div>
 
-      <form onSubmit={chatArgs.handleSubmit}>
+      <form
+        onSubmit={(formEvent) => {
+          chatModelA.handleSubmit(formEvent);
+          chatModelB.handleSubmit(formEvent);
+        }}
+      >
         <input
           className="bg-white text-black border border-solid border-black"
-          value={chatArgs.input}
+          value={chatModelA.input}
           placeholder="Send a message..."
-          onChange={chatArgs.handleInputChange}
-          disabled={chatArgs.isLoading}
+          onChange={(e) => {
+            chatModelA.handleInputChange(e);
+            chatModelB.handleInputChange(e);
+          }}
+          disabled={chatModelA.isLoading || chatModelB.isLoading}
         />
       </form>
     </div>
